770561ee228d1233989953602bbdbc1d4e8f6c5c
KAnanev <ananevdeveloper@yandex.ru>
2023-06-04 20:21:41 +0300

    Добавил простой тест

--- page_analyzer/tests/test_app.py
+++ page_analyzer/tests/test_app.py
@@ -1,2 +1,14 @@
-def test_home_page():
-    pass
+import pytest
+
+from page_analyzer.app import app
+
+
+@pytest.fixture()
+def client():
+    return app.test_client()
+
+
+def test_home_page(client):
+    response = client.get('/')
+    assert b'Hello, world!' in response.data
+    assert b'Buy, world!' not in response.data
