2c09c2ed881c619111e6e830d9285bc2f14fc653
KAnanev <russrage@yandex.ru>
2023-06-28 17:20:41 +0300

    Создана таблица url_checks, добавлены описания в database.sql

--- page_analyzer/database.sql
+++ page_analyzer/database.sql
@@ -5,3 +5,14 @@ CREATE TABLE IF NOT EXISTS urls (
     name TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE
 );
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INT,
+    status_code TEXT,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP WITH TIME ZONE,
+    FOREIGN KEY (url_id) REFERENCES urls (id)
+);
